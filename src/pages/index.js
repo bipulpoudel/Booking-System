1a346da32f76ca9a3abab7c75c9e6d63a588b91d
bipulpoudel <59637626+bipulpoudel@users.noreply.github.com>
2021-01-29 00:47:42 +0545

    [added] loading system added on button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import Button from "@components/common/Button";
 import Box from "@material-ui/core/Box";
 
 export default function Index() {
@@ -8,8 +9,14 @@ export default function Index() {
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
+          Booking System
         </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          text="Go to Dashboard"
+          href="/auth/login"
+        />
       </Box>
     </Container>
   );
