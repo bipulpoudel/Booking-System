1f527fbac5d23bd9b4b0b3f5dcbc467fce2fe676
bipulpoudel <59637626+bipulpoudel@users.noreply.github.com>
2021-02-09 00:17:00 +0545

    [added] disabled added for the booking

--- src/components/common/DayTimeline.js
+++ src/components/common/DayTimeline.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import { useDispatch } from "react-redux";
+
+import { getEvents } from "@api/event";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,6 +23,32 @@ const DayTimeline = ({ day, date, month, data, doctorId, timeline }) => {
   const dispatch = useDispatch();
 
   const router = useRouter();
+
+  const [events, setEvents] = useState([]);
+
+  const [found, setFound] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      let events = await getEvents();
+
+      setEvents(events);
+
+      setLoading(false);
+
+      for (let i = 0; i < events.length; i++) {
+        if (events[i].date == date) {
+          setFound(true);
+          break;
+        }
+      }
+    };
+
+    fetchEvents();
+  }, []);
 
   const bookEvent = (time) => {
     dispatch({
@@ -47,18 +75,23 @@ const DayTimeline = ({ day, date, month, data, doctorId, timeline }) => {
         {date} {month}
       </Typography>
 
-      {data?.map((time, index) => (
-        <Button
-          className={classes.margin}
-          variant="outlined"
-          size="small"
-          color="primary"
-          key={index}
-          onClick={() => bookEvent(time)}
-        >
-          {time.startTime}-{time.endTime}
-        </Button>
-      ))}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        data?.map((time, index) => (
+          <Button
+            className={classes.margin}
+            variant="outlined"
+            size="small"
+            color="primary"
+            key={index}
+            onClick={() => bookEvent(time)}
+            disabled={found}
+          >
+            {time.startTime}-{time.endTime}
+          </Button>
+        ))
+      )}
     </div>
   );
 };
